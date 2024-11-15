bc30a72c826c4d148f06cbcbf36c938b2248b4ce
codiguss <gustavo369bezerra@gmail.com>
2024-11-15 10:30:06 -0300

    Criando estilos

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider} from 'styled-components'
+
+import theme from './styles/theme'
+
 import { Details } from './pages/Details/index'
 
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
-    <Details />
+    <ThemeProvider theme={theme}>
+      <Details />
+    </ThemeProvider>
   </StrictMode>,
 )
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,13 +1,13 @@
-import "./styles.css";
+import { Container } from './styles'
 
 export function Details() {
 
   return(
     
-    <>
+    <Container>
       <h1>Hello World!</h1>
       <span>Gustavo Bezerra</span>
-    </>
+    </Container>
 
   )
 }
